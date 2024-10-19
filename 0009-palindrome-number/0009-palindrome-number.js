4e177846a175b1da0982bfc3952f8a04abe4b67c
mohammedrimshan <148178935+mohammedrimshan@users.noreply.github.com>
2024-10-19 09:01:08 +0530

    Time: 119 ms (88.35%), Space: 57.7 MB (54.56%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let str=x.toString();
+    let reverseStr='';
+    for(let i=str.length-1;i>=0;i--){
+        reverseStr=reverseStr+str[i];
+    }
+    return str===reverseStr;
+};
