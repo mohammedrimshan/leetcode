433cc340731a660267be305ee46f877750ce7bb6
mohammedrimshan <148178935+mohammedrimshan@users.noreply.github.com>
2024-10-26 17:15:02 +0530

    Time: 9 ms (95.32%), Space: 59.1 MB (10.24%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let str=x.toString();
+     let str=x.toString();
     let reverseStr='';
     for(let i=str.length-1;i>=0;i--){
         reverseStr=reverseStr+str[i];
