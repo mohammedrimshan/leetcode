39d2f3d0a5b7caefda73555176b3dafe7d6bd0ef
Mohammed rimshan K K <148178935+mohammedrimshan@users.noreply.github.com>
2025-04-22 15:54:01 +0530

    Time: 17 ms (48.39%), Space: 61 MB (12.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,8 +4,16 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-       if (s.length !== t.length) {
-        return false;
-    }
-    return s.split('').sort().join('') === t.split('').sort().join('');
+       if (s.length !== t.length) return false;
+        let obj = {}
+        for(let val of s){
+            obj[val] = (obj[val]||0) + 1;
+        }
+
+        for(let val of t){
+            if(!obj[val]) return false
+            obj[val]--;
+        }
+        
+    return true
 };
