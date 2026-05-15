baa2f78064b0cd822ba723f8be7520c025015bda
Mohammed rimshan K K <148178935+mohammedrimshan@users.noreply.github.com>
2026-05-15 10:05:09 +0530

    Time: 4 ms (51%), Space: 55.3 MB (64.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,24 +3,23 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    const stack = []
-    for(let i=0;i<s.length;i++){
-        const char = s[i];
-        if(char==='(' || char==='{'||char==='['){
-            stack.push(char)
-        }else if(char===')' || char==='}'||char===']'){
-            if(stack.length===0){
-                return false
-            }
-            const top = stack.pop()
-            if((char===")" && top !=="(") ||
-               (char==="]" && top !=="[") ||
-               (char==="}" && top !=="{")
-            ){
-                return false
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+            stack.push(s[i]);
+        } else {
+            let last = stack.pop();
+
+            if (
+                (s[i] === ")" && last !== "(") ||
+                (s[i] === "]" && last !== "[") ||
+                (s[i] === "}" && last !== "{")
+            ) {
+                return false;
             }
         }
     }
-    console.log(stack)
-    return stack.length===0
+
+    return stack.length === 0;
 };
